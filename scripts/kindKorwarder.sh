6d31017bb272dbd963b503a6b2f670c5059c27b7
DouglasKrouth <douglaskrouth@gmail.com>
2023-10-24 19:26:57 -0500

    Got args working, need to add func to generate exposed ports

--- scripts/kindKorwarder.sh
+++ scripts/kindKorwarder.sh
@@ -8,14 +8,14 @@ Help()
     echo "Syntax: kindKorwarder [-s|h|u|d]"
     
     echo "options:"
-    echo "s     Start a kind cluster with name "kind-korwarder""
+    echo "s     Attempt to start a kind cluster with name "kind-korwarder""
     echo "h     Print this Help (meta!)."
     echo "u     Get status of kind-korwarder local cluster"
     echo "d     Delete the kind-korwarder local cluster"
     echo
 }
 ################################################################################
-# Check for test deps
+# Check for test deps - kind and kubectl
 CheckDeps(){
 if ! command -v kind &> /dev/null
 then
@@ -28,23 +28,52 @@ then
     exit 1
 fi
 }
-
+################################################################################
 # Case for no args, return help
 [ $# -eq 0 ] && { Help; exit; }
 
-# Case for -h
-while getopts ":h" option; do
-   case $option in
-      h) # display Help
-         Help
-         exit;;
-   esac
-   exit 0
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -s|--start)
+    CheckDeps
+    kind create cluster --name korwarder
+    wait
+    if [ $? -eq 1 ]; then
+        echo "korwarder test k8's deployment already exists"
+    fi
+    # Shifts aren't really needed as we only take one arg but I like having them there
+    shift # past argument
+    shift # past value
+      ;;
+    -u|--status)
+    CheckDeps
+    kubectl cluster-info --context kind-korwarder
+    wait
+      shift
+      shift
+      ;;
+    # Goofy note about kind delete : It will print "..." if the cluster at spec. --name doesn't exist
+    -d|--delete)
+    CheckDeps
+    kind delete cluster --name korwarder
+    wait
+      shift
+      shift
+      ;;
+    -h|--help)
+    Help
+      shift
+      shift
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
 done
 
-
-kind create cluster --name korwarder
-if [ $? -eq 1 ]; then
-    echo "korwarder test k8's deployment already exists"
-fi
-Help
+exit 0
